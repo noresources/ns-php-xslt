3099ca63356a9e8c089f0a4ad09dabb47d450e8a
Renaud Guillard <dev@nore.fr>
2019-07-07 19:49:23 +0200

    Stylesheet::consolidate
    remove duplicated variable, param & template nodes

--- Stylesheet.php
+++ Stylesheet.php
@@ -27,7 +27,6 @@ class StylesheetException extends \Exception
 
 /**
  * XSLT stylesheet utility
- * 
  */
 class Stylesheet
 {
@@ -56,31 +55,31 @@ class Stylesheet
 		$context = new StylesheetContext($stylesheet, $documentDirectoryPath);
 		return self::consolidateStylesheet($context);
 	}
-	
+
 	/**
 	 * XSLT schema namespace URL
 	 * @var string
 	 */
 	const XSLT_NAMESPACE_URI = 'http://www.w3.org/1999/XSL/Transform';
-	
+
 	/**
 	 * XSLT namespace prefix used internally
 	 * @var string
 	 */
 	const XSLT_NAMESPACE_PREFIX = 'xsl';
-	
+
 	/**
 	 * Node name of the XSLT stylesheet root element
 	 * @var string
 	 */
 	const DOCUMENT_ROOT_ELEMENT = 'stylesheet';
-	
+
 	/**
 	 * XML namespace prefix used internally
 	 * @var string
 	 */
 	const XML_NAMESPACE_PREFIX = 'xml';
-	
+
 	/**
 	 * XSM namespace URL
 	 * @var string
@@ -88,7 +87,6 @@ class Stylesheet
 	const XML_NAMESPACE_URI = 'http://www.w3.org/XML/1998/namespace';
 
 	/**
-	 * 
 	 * @param StylesheetContext $context
 	 * @return \\DOMDocument
 	 */
@@ -103,7 +101,7 @@ class Stylesheet
 		{
 			self::consolidateStylesheetNode($context, $node);
 		}
-		
+
 		return $context->stylesheet;
 	}
 
@@ -116,11 +114,9 @@ class Stylesheet
 		{
 			throw new StylesheetException($href . ' not found');
 		}
-		
+
 		// Comment to mark there was a import node here
-		$context->stylesheet->documentElement->insertBefore(
-				$context->stylesheet->createComment('merged import href="' . $href . '"'), 
-				$node);
+		$context->stylesheet->documentElement->insertBefore($context->stylesheet->createComment('merged import href="' . $href . '"'), $node);
 
 		$impl = new \DOMImplementation();
 		$sub = $impl->createDocument(self::XSLT_NAMESPACE_URI, self::XSLT_NAMESPACE_PREFIX . ':' . self::DOCUMENT_ROOT_ELEMENT);
@@ -135,15 +131,70 @@ class Stylesheet
 		}
 
 		$node->parentNode->removeChild($node);
+		self::removeDuplicatedTemplates($context);
+	}
+
+	private static function removeDuplicatedTemplates(StylesheetContext $context)
+	{
+		$xpath = new \DOMXPath($context->stylesheet);
+		$xpath->registerNamespace(self::XSLT_NAMESPACE_PREFIX, self::XSLT_NAMESPACE_URI);
+
+		$nodeNames = array (
+				'template' => array (
+						'name',
+						'match'
+				),
+				'param' => array (
+						'name'
+				),
+				'variable' => array (
+						'name'
+				)
+		);
+
+		foreach ($nodeNames as $nodeName => $attributes)
+		{
+			$nodes = $xpath->query('./' . self::XSLT_NAMESPACE_PREFIX . ':' . $nodeName);
+			foreach ($nodes as $node)
+			{
+				$attributeName = false;
+				foreach ($attributes as $name)
+				{
+					if ($node->hasAttribute($name))
+					{
+						$attributeName = $name;
+						break;
+					}
+				}
+			
+
+				if ($attributeName)
+				{
+					$query = './' . self::XSLT_NAMESPACE_PREFIX . ':'.$nodeName.'[@' . $attributeName . '="' . $node->getAttribute($attributeName) . '"]';
+					$duplicates = $xpath->query($query);
+					if ($duplicates->length > 1)
+					{
+						$c = 0;
+						for ($i = 0; $i < $duplicates->length -1; $i++)
+						{
+							$duplicated = $duplicates->item($i);
+							$duplicated->parentNode->removeChild ($duplicated);
+						}
+					}
+				}
+			}
+		}
 	}
 }
 
 class StylesheetContext
 {
+
 	public $stylesheet;
+
 	public $documentDirectoryPath;
-	
-	public function __construct (\DOMDocument $stylesheet, $documentDirectoryPath)
+
+	public function __construct(\DOMDocument $stylesheet, $documentDirectoryPath)
 	{
 		$this->stylesheet = $stylesheet;
 		$this->documentDirectoryPath = $documentDirectoryPath;
